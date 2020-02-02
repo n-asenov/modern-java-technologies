31a17904e2f2410110c06f14589588531c650785
Nikolay Asenov <asenov.nikolay98@gmail.com>
2020-02-03 01:49:32 +0200

    add client input parser

---
+++ FoodAnalyzerClient/src/bg/sofia/uni/mjt/input/parser/InputParser.java
@@ -0,0 +1,23 @@
+package bg.sofia.uni.mjt.input.parser;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class InputParser {
+
+    public String getCommand(String input) {
+        String[] tokens = input.trim().split(" ");
+        int commandIndex = 0;
+        return tokens[commandIndex];
+    }
+    
+    public List<String> getArguments(String clientMessage) {
+        String[] tokens = clientMessage.trim().split(" ");
+        
+        return Stream.of(tokens)
+                .skip(1)
+                .collect(Collectors.toList());
+    }
+    
+}
---
+++ FoodAnalyzerClient/test/bg/sofia/uni/mjt/input/parser/InputParserTest.java
@@ -0,0 +1,58 @@
+package bg.sofia.uni.mjt.input.parser;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class InputParserTest {
+
+    private InputParser parser;
+
+    @Before
+    public void initialize() {
+        parser = new InputParser();
+    }
+
+    @Test
+    public void testGetCommandWithEmptyInput() {
+        String emptyInput = "";
+        assertEquals(emptyInput, parser.getCommand(emptyInput));
+    }
+
+    @Test
+    public void testGetArgumentsWithEmptyInput() {
+        String emptyInput = "";
+
+        assertEquals(List.of(), parser.getArguments(emptyInput));
+    }
+
+    @Test
+    public void testGetCommandWithInputWithOneWord() {
+        String input = "test";
+        assertEquals(input, parser.getCommand(input));
+    }
+
+    @Test
+    public void testGetCommandWithInputWithSeveralWords() {
+        String input = "test test";
+        String expectedResult = "test";
+        assertEquals(expectedResult, parser.getCommand(input));
+    }
+
+    @Test
+    public void testGetArgumentsWithInputWithOneWord() {
+        String input = "test";
+        assertEquals(List.of(), parser.getArguments(input));
+    }
+
+    @Test
+    public void testGetArgumentsWithInputWithSeveralWords() {
+        String input = "test test test";
+        List<String> expectedResult = List.of("test", "test");
+        assertEquals(expectedResult, parser.getArguments(input));
+    }
+
+}
